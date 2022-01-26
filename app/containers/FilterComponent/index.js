2cb5579caef5facbc24cd0a1f7beedab4e1b66a7
ks-mani <kumar.mani017@gmail.com>
2022-01-27 02:12:13 +0530

    Button Submit - 3

--- app/containers/FilterComponent/index.js
+++ app/containers/FilterComponent/index.js
@@ -8,13 +8,29 @@ import React, { memo, useCallback } from 'react';
 import { connect } from 'react-redux';
 import { compose } from 'redux';
 import { Divider } from 'antd';
+import axios from 'axios';
 
 import FilterSearchBarComponent from '../FilterSearchBarComponent/index';
 
 export function FilterComponent() {
   const searchTheRecords = useCallback((agents, duration) => {
-    console.log(agents);
-    console.log(duration);
+    const headers = {
+      'Content-Type': 'application/json'
+    }
+
+    const payload = {
+      info: {
+        filter_agent_list: agents,
+        filter_time_range: [0, duration]
+      }
+    }
+    axios.post('https://damp-garden-93707.herokuapp.com/getfilteredcalls', JSON.stringify(payload) , {headers})
+      .then((resp)=>{
+        console.log(resp)
+      })
+      .catch(err=>{
+        console.log(err)
+      })
   }, []);
   return (
     <>
