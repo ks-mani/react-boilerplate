1bd78653fe5e2bafb33e5d3302da6fcb9b75bc4c
ks-mani <kumar.mani017@gmail.com>
2022-01-27 13:31:03 +0530

    Label - 1

--- app/containers/LabelContainer/index.js
+++ app/containers/LabelContainer/index.js
@@ -8,12 +8,35 @@ import React, { memo } from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import { compose } from 'redux';
+import { Table, Button } from 'antd';
+
+const columns = [
+  {
+    title: 'Call Id',
+    dataIndex: 'call_id',
+    defaultSortOrder: 'descend',
+    sorter: (a, b) => parseInt(a.call_id) - parseInt(b.call_id),
+  },
+  {
+    title: 'Labels',
+    dataIndex: 'label_id',
+    // render: (val)=>parseFloat(val).toFixed(2).toString()
+  },
+];
 
 export function LabelContainer() {
   return (
-    <div>
-      <p>Label Container</p>
-    </div>
+    <>
+      <div style={{ marginBottom: 16 }}>
+          <Button type="primary">
+            Add/Remove Tags
+          </Button>
+          <span style={{ marginLeft: 8 }}>
+            {/* {hasSelected ? `Selected ${selectedRowKeys.length} items` : ''} */}
+          </span>
+        </div>
+        <Table  columns={columns} dataSource={[]} scroll={{ y: 400 }} />
+    </>
   );
 }
 
